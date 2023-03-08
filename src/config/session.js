a94df42af678818f93c840be59d3d81e5c1cc0ff
zeam2003 <zeam2003@outlook.com>
2023-03-08 19:43:06 -0300

    final ent

--- src/config/session.js
+++ src/config/session.js
@@ -1,10 +1,27 @@
-const sessionConfig = {
-    secret: 'soyunomasdelmonton',
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            maxAge: 60000
-        },
-}
+import MongoStore from "connect-mongo";
+import session from "express-session";
+import * as dotenv from 'dotenv';
 
-export default sessionConfig;
+dotenv.config();
+
+// Conectamos a Mongo
+const sessionStore = MongoStore.create({
+    mongoUrl: process.env.MONGO,
+});
+
+
+// Configuramos los parametros de la session
+export default session({
+    store: sessionStore,
+    secret: 'EstoEsUnSecretoNoDivulgar',
+    resave: true,
+    rolling: false,
+    saveUninitialized: false,
+    unset: 'destroy',
+    cookie: {
+        sameSite: 'none',
+        secure: true,
+        httpOnly: true,
+        maxAge: 8600000
+    },
+});
